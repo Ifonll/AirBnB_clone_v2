dbb644fdb6e745369da66785b6ef215e7f7ec246
marwa <Ifonll>
2024-06-08 09:22:55 -0400

    a

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+""""flask starts a web application"""
+from flask import Flask
+
+
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def saying_hello():
+    """"dispplays hello"""
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb", strict_slashes=False)
+def hbnb():
+    """"displays hbnb"""
+    return "HBNB"
+
+
+@app.route("/c/<text>", strict_slashes=False)
+def aboutC(text):
+    """c is so good"""
+    modified_text = text.replace('_', ' ')
+    return f"C {modified_text}"
+
+
+@app.route("/python/<text>", strict_slashes=False)
+def aboutPython(text):
+    """"python is amazing"""
+    modified_text = text.replace('_', ' ')
+    return f"Python {modified_text}"
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
