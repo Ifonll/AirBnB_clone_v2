566b2c0daf939ceb71009b3b0f29dc7c7f108781
marwa <Ifonll>
2024-06-08 10:56:01 -0400

    a

---
+++ web_flask/5-number_template.py
@@ -0,0 +1,49 @@
+#!/usr/bin/python3
+""""flask starts a web application"""
+from flask import Flask, render_template
+
+
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def saying_hello():
+    """"dispplays hello"""
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb", strict_slashes=False)
+def hbnb():
+    """"displays hbnb"""
+    return "HBNB"
+
+
+@app.route("/c/<text>", strict_slashes=False)
+def aboutC(text):
+    """c is so good"""
+    modified_text = text.replace('_', ' ')
+    return f"C {modified_text}"
+
+
+@app.route("/python/", defaults={'text': 'is cool'}, strict_slashes=False)
+@app.route("/python/<text>", strict_slashes=False)
+def aboutPython(text):
+    """"python is amazing"""
+    modified_text = text.replace('_', ' ')
+    return f"Python {modified_text}"
+
+
+@app.route("/number/<int:n>", strict_slashes=False)
+def aboutnumbers(n):
+    """display number"""
+    return f"{n} is a number"
+
+
+@app.route("/number_template/<int:n>", strict_slashes=False)
+def aboutHTML(n):
+    """"display html content"""
+    return render_template("5-number.html", n=n)
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0")
